fd1131875171c512b3b37e4b8c829e15919683c4
Mattazz <matthewazada101@gmail.com>
2023-03-31 22:22:05 +0800

    hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createHashRouter, RouterProvider } from 'react-router-dom'
 // Components
 import App from './App'
 import {Error, Help} from './components'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: <App />,
